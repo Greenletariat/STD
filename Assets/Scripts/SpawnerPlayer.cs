3b95cbc58e4aaead336460a666e0469426a7b75e
Nore5515 <Nore5515@gmail.com>
2016-12-07 11:28:12 -0600

    Testing merge branch

--- Assets/Scripts/SpawnerPlayer.cs
+++ Assets/Scripts/SpawnerPlayer.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerSpawner : MonoBehaviour {
+public class SpawnerPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Test1_GitTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1_GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
