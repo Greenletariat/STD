bc83f25c81d1f94d4d5fac1aab1dd75050a662c8
Nore5515 <Nore5515@gmail.com>
2016-12-09 10:56:16 -0600

    fixed shipselector

--- Assets/Scripts/SpawnerPlayer.cs
+++ Assets/Scripts/SpawnerPlayer.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SpawnerPlayer : MonoBehaviour {
 
+	GameObject shipSelectorCanvas;
+	Button b1;
+
 	// Use this for initialization
 	void Start () {
-	
+		shipSelectorCanvas = GameObject.Find("ShipSelectorCanvas");
+		if (shipSelectorCanvas == null){
+			Debug.Log("ShipSelectorCanvas not initialized!: " + this.ToString());
+		}
+
+		b1 = shipSelectorCanvas.transform.GetChild(0).GetComponent<Button>();
+		if (b1 == null){
+			Debug.Log("Button for ShipSelectorCanvas not initalized!: "+ this.ToString());
+		}
+		b1.onClick.AddListener(delegate() {ShipSelected();});
+
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void ShipSelected(){
+		shipSelectorCanvas.SetActive(false);
 	}
+
 }
